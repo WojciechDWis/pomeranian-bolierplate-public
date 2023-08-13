f6f8c6bff046d82183914111e19ff97251e39490
Wojciech D W <w.d.wisniewski@gmail.com>
2023-08-13 11:44:59 +0200

    json i xml i storage i cookies

---
+++ src/App/Exercises/Js/json_xml_storage/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'json_xml_storage',
+  date: '13-08-2023',
+  linkLabel: 'json_xml_storage',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['json', 'xml', 'storage', 'cookies', 'sessionStorage'],
+};
